c61ae0af6ec2b50112118e8029aec8c0dd62cc7f
unknown <krishtopiwala1724@gmail.com>
2025-01-23 07:03:50 +0530

    updated commit

--- basic-projects/scripts/bmi_calculator.js
+++ basic-projects/scripts/bmi_calculator.js
@@ -13,6 +13,13 @@ form.addEventListener('submit', function(e) {
         results.innerHTML = `please give a valid height ${height}`;
     } else {
         const bmi = (weight / ((height * height) / 10000)).toFixed(2);
-        results.innerHTML = `<span>${bmi}</span>`;
+        results.innerHTML = `<h3>${bmi}</h3>`;
+        if (bmi < 18.6) {
+            results.innerHTML = `<span><b> You are in <mark>under weight </mark>category</b></span>`;
+        } else if (bmi > 18.6 && bmi < 24.9) {
+            results.innerHTML = `<span><b>You are in <mark>normal range</mark></b>category</span>`;
+        } else if (bmi > 24.9) {
+            results.innerHTML = `<span><b>You are in <mark>Overweight</mark></b>category</span>`;
+        }
     }
 });
